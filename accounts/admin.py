0b164d329a2fc622ba07c5ba5cfe29d3519de7bb
dinesh-cyber <dineshmandava0@gmail.com>
2020-09-09 16:30:41 +0530

    user table

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Faculty, Students
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Faculty)
+admin.site.register(Students)
--- accounts/models.py
+++ accounts/models.py
@@ -18,20 +18,24 @@ class User(AbstractUser):
         ("BioTech", "BioTech"),
     ]
 
-    firstname=models.CharField(max_length=50)
-    lasttname=models.CharField(max_length=50)
-    email=models.EmailField(max_length=20)
+    username=models.CharField(max_length=50, unique=True)
+    email=models.EmailField(unique=True)
     gender=models.CharField(max_length=10, choices=gender)
     password=models.CharField(max_length=300)
-    branch=models.CharField(max_length=50)
-    mobile=models.CharField(max_length=10)
-    rollno=models.CharField(max_length=20)
-    usertype=models.CharField(max_length=20,choices=type_user)
+    branch=models.CharField(max_length=50, choices=branch_choices)
+    contact=models.BigIntegerField(unique=True, null = True)
+    user_type=models.CharField(max_length=20,choices=type_user, default = "student")
 
     class Meta:
         db_table = "user"
 
 
-    
+class Faculty(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    employee_id = models.CharField(max_length=30)
 
-
+class Students(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_name = models.CharField(max_length = 30)
+    roll_no=models.CharField(max_length=20, unique=True)
